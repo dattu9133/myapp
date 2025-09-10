6506a744b0ddbca3e0739e83484d8d57262f23af
dattasaimoganti <dattasaimoganti@gmail.com>
2025-09-10 15:43:12 +0000

    hi

--- android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.myapp
 
+import android.os.Bundle
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "onCreate: FlutterActivity is being created.")
+    }
+}
